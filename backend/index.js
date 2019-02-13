f0b4c1b0334657933f11802c2b666c02cb2dcc55
Sudarshan baliga <baliga108@gmail.com>
2019-02-13 08:51:57 +0530

    backend init

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var api = require("./api");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));  
+
+app.use("/", api);
+
+app.use(express.static("./build"));
+app.listen(process.env.PORT || 3001, () => {
+    console.log("listening to port 3001");
+});
