1b7359a06eeccdf1c9d566a6d2eeebae45abb30d
megashrieks <shrikanthbudya@gmail.com>
2019-02-13 15:21:06 +0530

    added get route information

---
+++ src/components/GetRouteInformation/GetRouteInformation.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import "./GetRouteInformation.css";
+import { CustomInput, Button } from "../../library";
+export default class GetRouteInformation extends Component {
+	state = {
+		from: "",
+		fromerror: false,
+		to: "",
+		toerror: "",
+		step: 1,
+		error: false
+	};
+	change = field => val => {
+		return new Promise(resolve => {
+			console.log(field, val);
+			this.setState(
+				prevstate => ({
+					[field]: val,
+					[field + "error"]: val.length === 0,
+					error: val.length === 0
+				}),
+				resolve()
+			);
+		});
+	};
+	proceed = () => {
+		let errors = ["from", "to"];
+		this.change(errors[this.state.step - 1])(
+			this.state[errors[this.state.step - 1]]
+		).then(() => {
+			if (this.state.error) return;
+			console.log(this.state.error);
+			this.setState(prevstate => ({
+				step: prevstate.step + 1
+			}));
+		});
+	};
+	render() {
+		return (
+			<div className="centered">
+				{this.state.step === 1 && (
+					<div className="step-1">
+						<CustomInput
+							label="From"
+							defaultValue=""
+							watcher={this.change("from")}
+							triggerOnChange={true}
+							error={this.state.fromerror}
+							errorMessage={"Cannot be empty"}
+						/>
+					</div>
+				)}
+
+				{this.state.step === 2 && (
+					<div className="step-2">
+						<CustomInput
+							label="To"
+							defaultValue=""
+							watcher={this.change("to")}
+							triggerOnChange={true}
+							error={this.state.toerror}
+							errorMessage={"Cannot be empty"}
+						/>
+					</div>
+				)}
+				<div style={{ textAlign: "right" }}>
+					<Button color="info" size="vsmall" onClick={this.proceed}>
+						Next
+					</Button>
+				</div>
+			</div>
+		);
+	}
+}
