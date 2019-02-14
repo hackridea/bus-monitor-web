8a7dd1ecc6a519b610919a7c759f32ca16683693
Sudarshan baliga <baliga108@gmail.com>
2019-02-14 10:43:38 +0530

    before crowd

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ var app = express();
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var api = require("./api");
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -10,10 +13,23 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+io.on('connection', function (socket) {
+    socket.emit('test', { hello: 'world' });
+});
 
+app.post("/bus/currentData", (req, res) => {
+    console.log("current data of the bus", req.body);
+    let id = req.body.id, routeid = req.body.routeid, lat = req.body.lat, lng = req.body.lng;
+    io.sockets.emit("currentData", { id: id, routeid: routeid, lat: lat, lng: lng });
+    res.send({success: true});
+})
 app.use("/", api);
 
 app.use(express.static("./build"));
-app.listen(process.env.PORT || 3001, () => {
-    console.log("listening to port 3001");
+
+
+
+
+server.listen(3001,()=>{
+    console.log('listening at 3000');
 });
